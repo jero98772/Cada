22fa3b04fb9a93a4562a75d373372d6a58b1f4c9
jero98772 <jero98772@protonmail.com>
2025-09-03 12:35:21 -0500

    update

---
+++ CADA/src/main/java/com/example/CADA/CadaApplication.java
@@ -0,0 +1,13 @@
+package com.example.CADA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadaApplication.class, args);
+	}
+
+}
